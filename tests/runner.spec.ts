624d02b3b99349514b7e8a06822f14b82a40380f
sibiraj-s <dev.sibiraj@outlook.com>
2021-12-26 02:03:47 +0530

    Tidy runner tests

--- tests/runner.spec.ts
+++ tests/runner.spec.ts
@@ -1,6 +1,8 @@
 import fs from 'node:fs';
 import path from 'node:path';
-import { afterEach, describe, expect, it, jest } from '@jest/globals';
+import {
+  afterEach, beforeEach, describe, expect, it, jest,
+} from '@jest/globals';
 import * as core from '@actions/core';
 
 import run from '../src/runner';
@@ -11,6 +13,10 @@ const mockEndGroup = jest.spyOn(core, 'endGroup');
 const mockSetOutput = jest.spyOn(core, 'setOutput');
 
 describe('Runner', () => {
+  beforeEach(() => {
+    process.env['INPUT_WORKING-DIRECTORY'] = '.tmp';
+  });
+
   afterEach(async () => {
     jest.restoreAllMocks();
     await fs.promises.rm(tempy.root, { force: true, recursive: true });
@@ -21,7 +27,6 @@ describe('Runner', () => {
     process.env.INPUT_PATH = '*';
     process.env.INPUT_OUTPUT = 'runner.zip';
     process.env.INPUT_GZIP = 'false';
-    process.env['INPUT_WORKING-DIRECTORY'] = '.tmp';
 
     await run();
     const outfile = path.join(process.cwd(), '.tmp/runner.zip');
@@ -40,7 +45,6 @@ describe('Runner', () => {
     process.env.INPUT_FORMAT = 'zip';
     process.env.INPUT_PATH = '*';
     process.env.INPUT_OUTPUT = 'runner_a.zip';
-    process.env['INPUT_WORKING-DIRECTORY'] = '.tmp';
 
     await run();
     const outfile = path.join(process.cwd(), '.tmp/runner_a.zip');
